cf075284438a614ff15a98c647bac760d652512d
Mertkan <MertkanEvran@outlook.com>
2023-04-26 17:08:09 +0300

    Optimization in user_login

--- user_login.py
+++ user_login.py
@@ -3,45 +3,64 @@ def interface():
     print("1-) Login")
     print("2-) Register")
     print("3-) Quit")
-    while True:
-        user_choice = int(input("Please select a operation: "))
-        if user_choice == 1:
-            user_login()
-            break
-        elif user_choice == 2:
-            user_register()
-            break
-        elif user_choice == 3:
-            break
-        else:
-            raise Exception("Please enter a accaptable value.")
-        
-def check_file():
+    user_choice = int(input("Please select a operation: "))
+    if user_choice == 1:
+        user_login()
+    elif user_choice == 2:
+        user_register()
+    elif user_choice == 3:
+        exit()
+    else:
+        raise Exception("Please enter a accaptable value.")
+            
+       
+def check_file(file_name):
     try:
-        open("user_database.txt","r",encoding="utf-8")
+        open(file_name,"r",encoding="utf-8")
     except FileNotFoundError:
-        with open("user_database.txt","x",encoding="utf-8") as folder:
-            print("File is created")
-            return True
+        return False
     else:
         return True
+
+def create_file(file_name):
+    if check_file(file_name):
+        print("File is already exist")
+    else:
+        folder = open(file_name,"x",encoding="utf-8")
+        folder.close()
+        print("File is created")
+        interface()
+
+def initilaze_system(file_name):
+    if check_file(file_name):
+        interface()
+    else:
+        print("Need a file to store the user's data. Do you want to create a new one")
+        print("1-) Yes")
+        print("2-) No")
+        user_input = input("Please select your option: ")
+        if user_input == 1:
+            create_file(file_name)
+        elif user_input == 2:
+            exit()
+        else:
+            raise Exception("Not acceptable input.")
 
 def get_data():
     username=input("Username: ")
     password = input("Password: ")
-
     return username,password
 
 def user_validation(username,password):
-    if check_file():
+    if check_file("user_database.txt"):
         with open("user_database.txt","r",encoding="utf-8") as folder:
             content = folder.readlines()
             for line in content:
                 user_info = line.strip().split("->")
                 if username == user_info[0] and password == user_info[1]:
                     return True
-            return False
-
+            return False    
+        
 def user_login():
     user_info=get_data()
     username = user_info[0]
@@ -57,7 +76,7 @@ def user_login():
         elif result=="n":
             interface()
         else:
-            raise Exception("Please enter the right input")
+            raise Exception("Not acceptable input.")
 
 def user_register():
     user_info = get_data()
@@ -70,5 +89,9 @@ def user_register():
         with open("user_database.txt","a",encoding="utf-8") as folder:
             folder.write(username + "->" + password + "\n")
             print("User is registered succesfully")
-    
-interface()
+
+while True:
+    try:
+        initilaze_system("user_database.txt")
+    except Exception as ex:
+        print(ex)
